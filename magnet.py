61d1f50ecc6c5ac58c1f7eba8c17f41d59fdb239
Simon Quain <simon.quain@gmail.com>
2012-01-21 17:55:20 +0000

    Basic Sprite class added

--- magnet.py
+++ magnet.py
@@ -5,8 +5,46 @@
 
 import pygame
 from pygame.locals import *
+import random
 
 SCREENRECT = Rect(0,0,640,480)
+
+
+
+class Sprite:
+   """
+The base Sprite class for creating / moving around characters
+   
+   Polarity is boolean - 
+      True = Positive
+      False = Negative
+      
+   """
+   def __init__(self, xpos, ypos, polarity, filename):
+      self.x = xpos
+      self.y = ypos
+      self.bitmap = image.load(filename).convert()
+      self.bitmap.set_colorkey((255,0,255))
+      self.xmoveamount = random.randint(-3, 3)
+      self.ymoveamount = random.randint(1,3)
+
+   def set_position(self, xpos, ypos):
+      self.x = xpos
+      self.y = ypos
+
+   def render(self):
+      screen.blit(self.bitmap, (self.x, self.y))
+
+
+   
+
+
+
+
+
+
+
+
 
 def main():
    pygame.init
