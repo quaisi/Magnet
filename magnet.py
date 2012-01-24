fd2f112bc63ded994486da379f79c9fb9fae694a
Simon Quain <simon.quain@gmail.com>
2012-01-24 23:29:37 +0000

    Workable mergeable code

--- magnet.py
+++ magnet.py
@@ -38,7 +38,7 @@ def load_sound(name):
    return sound
 
 
-# Classes start here -------------------------------------------
+# Classes start here ----------------------------------------
 
 
 class Hero(pygame.sprite.Sprite):
@@ -97,8 +97,8 @@ class Enemy(pygame.sprite.Sprite):
 
       if self.polarity == True and Hero.polarity == True:
 
-         self.x = self.x + self.xmoveamount
-         self.y = self.y + self.ymoveamount
+         self.x = self.x - self.xmoveamount
+         self.y = self.y - self.ymoveamount
       elif self.polarity == True and Hero.polarity == False:
          self.x -= dx / 10
          self.y -= dy / 10
@@ -106,8 +106,8 @@ class Enemy(pygame.sprite.Sprite):
          self.x -= dx / 10
          self.y -= dx / 10
       else:
-         self.x = self.x + self.xmoveamount
-         self.y = self.y + self.ymoveamount
+         self.x = self.x - self.xmoveamount
+         self.y = self.y - self.ymoveamount
          
       self.rect.midtop = (self.x,self.y)
       
